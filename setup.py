c202ca0a6a1cbc055d457a7d4cbb916f10fe37d3
rodrigo.refline <rodrigo.ristow@refline.ch>
2020-05-01 19:31:42 +0200

    Release 0.4

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.1'
+version = '0.4'
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='z3c.rest_publisher',
       version=version,
       description="This product has the base classes to implement a REST-Server using the concepts of Zope-traverser (IBrowserPublisher)",
-      long_description=open("README.md").read() + "\n" +
-                       open("HISTORY.txt").read(),
+      long_description=(
+              read('README.md')
+              + '\n\n' +
+              read('CHANGES.txt')
+      ),
       long_description_content_type='text/x-rst',
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
